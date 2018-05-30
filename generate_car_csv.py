48c7dadae5f082c575c1a99e476771340f0d6a4c
Konstantin Krastev <grizmin@gmail.com>
2018-05-30 15:11:51 +0300

    Beutify Added TODO

--- generate_car_csv.py
+++ generate_car_csv.py
@@ -4,7 +4,13 @@ import io
 from collections import defaultdict
 import time
 
+
 class CarGenerator:
+    """
+    Generates and writes car csv files from a given csv database.
+    Note: The csv database must contain the following fields:
+    trim,make,model,generation,body,drive,gearbox,engine_type,engine_volume,engine_power,year
+    """
 
     def __init__(self, file, folder='cars', fieldnames=None):
         self.folder = folder
@@ -23,13 +29,19 @@ class CarGenerator:
         :param csvobject: csv.DictReader object
         :return car_info: defaultdict object
         """
-
         car_info = defaultdict(list)
         for row in csvobject:
             car_info[row['make']].append(row)
         return car_info
 
     def write_car_csv(self, carsdict, make=None):
+        """
+        Writes Car CSV files to a given folder.
+
+        :param carsdict: defaultdict object - output from get_car_info
+        :param make: optional make parameter, sting, in case you want to generate only specific make
+        :return:
+        """
         # for carmake in carsdict.keys():
         makes = [make] if make else carsdict.keys()
         if not os.path.exists(self.folder):
@@ -38,29 +50,39 @@ class CarGenerator:
         for carmake in makes:
             with open(self.folder + os.sep + '{}.csv'.format(carmake), 'w', newline='') as csvfile:
                 if not self.fieldnames:
-                    self.fieldnames = ['Model', 'Year', 'Generation', 'Engine', 'Engine Type', 'Car Body', 'Drive', 'Gearbox']
+                    self.fieldnames = ['Model', 'Year', 'Generation', 'Engine', 'Engine Type', 'Car Body',
+                                       'Drive', 'Gearbox']
                 writer = csv.DictWriter(csvfile, fieldnames=self.fieldnames)
 
                 writer.writeheader()
                 for row in carsdict[carmake]:
                     # print(row)
-                    writer.writerow({'Model': row['model'], 'Year': row['year'], 'Engine': row['trim'], 'Generation': row['generation'],
-                                     'Engine Type': row['engine_type'], 'Car Body': row['body'], 'Drive': row['drive'],
-                                     'Gearbox': row['gearbox']})
+                    writer.writerow({'Model': row['model'], 'Year': row['year'], 'Engine': row['trim'], 'Generation':
+                                    row['generation'], 'Engine Type': row['engine_type'], 'Car Body': row['body'],
+                                    'Drive': row['drive'], 'Gearbox': row['gearbox']})
             print('{}.csv written.'.format(carmake))
+        return
 
     def run(self, make=None):
+        """
+        Self explainatory
+
+        :param make: Optional Make. Use it if you want to generate only 1 make instad of all.
+        :return: time taken
+        """
         start = time.time()
-        self.write_car_csv(self.get_car_info(self.cars),make=make)
+        self.write_car_csv(self.get_car_info(self.cars), make=make)
         stop = time.time()
         time_taken = stop - start
         return time_taken
 
+
 if __name__ == '__main__':
     import argparse
     parser = argparse.ArgumentParser()
-    parser.add_argument('-f', '--infile', type=argparse.FileType('r', encoding='utf8'), default=None, help="In file in CSV format" )
+    parser.add_argument('-f', '--infile', type=argparse.FileType('r', encoding='utf8'), default=None,
+                        help="In file in CSV format")
     args = parser.parse_args()
 
-gencars = CarGenerator('auto_databases_one_March_2018_en.csv')
-print('[*] Time taken: {0:.2f} seconds.'.format(gencars.run()))
+    gencars = CarGenerator('auto_databases_one_March_2018_en.csv')
+    print('[*] Time taken: {0:.2f} seconds.'.format(gencars.run()))
