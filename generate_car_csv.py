e23ff356aea6363f3743c434a20c5eb1f0a46926
Konstantin Krastev <grizmin@gmail.com>
2018-05-30 13:58:06 +0300

    ver 1.0

--- generate_car_csv.py
+++ generate_car_csv.py
@@ -2,32 +2,59 @@ import os
 import csv
 import io
 from collections import defaultdict
+import time
 
-def get_car_info(csvobject):
-    """
-    Generate dictionary with 'make' as key
-    :param csvobject: csv.DictReader object
-    :return car_info: defaultdict object
-    """
+class CarGenerator:
 
-    car_info = defaultdict(list)
-    for row in csvobject:
-        car_info[row['make']].append(row)
-    return car_info
+    def __init__(self, file, folder='cars', fieldnames=None):
+        self.folder = folder
+        self.fieldnames = fieldnames
+        try:
+            with open(file, encoding="utf8", newline="") as fh:
+                scars = fh.read()
+            self.cars = csv.DictReader(io.StringIO(scars), delimiter=',', dialect='excel')
+        except Exception:
+            print("Ops. There is something wrong accessing {}".format(file))
+            raise
 
-def write_car_csv(carsdict):
-    # for carmake in carsdict.keys():
-    for carmake in ['Audi']:
-        with open('{}.csv'.format(carmake), 'w', newline='') as csvfile:
-            fieldnames = ['Model', 'Year', 'Generation', 'Engine', 'Engine Type', 'Car Body', 'Drive', 'Gearbox']
-            writer = csv.DictWriter(csvfile, fieldnames=fieldnames)
+    def get_car_info(self, csvobject):
+        """
+        Generate dictionary with 'make' as key
+        :param csvobject: csv.DictReader object
+        :return car_info: defaultdict object
+        """
 
-            writer.writeheader()
-            for row in carsdict[carmake]:
-                print(row)
-                writer.writerow({'Model': row['model'], 'Year': row['year'], 'Engine': row['trim'], 'Generation': row['generation'],
-                                 'Engine Type': row['engine_type'], 'Car Body': row['body'], 'Drive': row['drive'],
-                                 'Gearbox': row['gearbox']})
+        car_info = defaultdict(list)
+        for row in csvobject:
+            car_info[row['make']].append(row)
+        return car_info
+
+    def write_car_csv(self, carsdict, make=None):
+        # for carmake in carsdict.keys():
+        makes = [make] if make else carsdict.keys()
+        if not os.path.exists(self.folder):
+            os.makedirs(self.folder)
+
+        for carmake in makes:
+            with open(self.folder + os.sep + '{}.csv'.format(carmake), 'w', newline='') as csvfile:
+                if not self.fieldnames:
+                    self.fieldnames = ['Model', 'Year', 'Generation', 'Engine', 'Engine Type', 'Car Body', 'Drive', 'Gearbox']
+                writer = csv.DictWriter(csvfile, fieldnames=self.fieldnames)
+
+                writer.writeheader()
+                for row in carsdict[carmake]:
+                    # print(row)
+                    writer.writerow({'Model': row['model'], 'Year': row['year'], 'Engine': row['trim'], 'Generation': row['generation'],
+                                     'Engine Type': row['engine_type'], 'Car Body': row['body'], 'Drive': row['drive'],
+                                     'Gearbox': row['gearbox']})
+            print('{}.csv written.'.format(carmake))
+
+    def run(self, make=None):
+        start = time.time()
+        self.write_car_csv(self.get_car_info(self.cars),make=make)
+        stop = time.time()
+        time_taken = stop - start
+        return time_taken
 
 if __name__ == '__main__':
     import argparse
@@ -35,12 +62,5 @@ if __name__ == '__main__':
     parser.add_argument('-f', '--infile', type=argparse.FileType('r', encoding='utf8'), default=None, help="In file in CSV format" )
     args = parser.parse_args()
 
-    if not args.infile:
-        with open("auto_databases_one_March_2018_en.csv", encoding="utf8", newline="") as fh:
-            cars = fh.read()
-    else:
-        cars = infile.read()
-
-    reader = csv.DictReader(io.StringIO(cars), delimiter=',', dialect='excel')
-
-    write_car_csv(get_car_info(reader))
+gencars = CarGenerator('auto_databases_one_March_2018_en.csv')
+print('[*] Time taken: {0:.2f} seconds.'.format(gencars.run()))
