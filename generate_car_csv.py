ed317f7785884f507b7cd8c5086bb88f43792891
Konstantin Krastev <grizmin@gmail.com>
2018-05-30 12:31:42 +0300

    daily backup

--- generate_car_csv.py
+++ generate_car_csv.py
@@ -15,16 +15,32 @@ def get_car_info(csvobject):
         car_info[row['make']].append(row)
     return car_info
 
+def write_car_csv(carsdict):
+    # for carmake in carsdict.keys():
+    for carmake in ['Audi']:
+        with open('{}.csv'.format(carmake), 'w', newline='') as csvfile:
+            fieldnames = ['Model', 'Year', 'Generation', 'Engine', 'Engine Type', 'Car Body', 'Drive', 'Gearbox']
+            writer = csv.DictWriter(csvfile, fieldnames=fieldnames)
+
+            writer.writeheader()
+            for row in carsdict[carmake]:
+                print(row)
+                writer.writerow({'Model': row['model'], 'Year': row['year'], 'Engine': row['trim'], 'Generation': row['generation'],
+                                 'Engine Type': row['engine_type'], 'Car Body': row['body'], 'Drive': row['drive'],
+                                 'Gearbox': row['gearbox']})
+
 if __name__ == '__main__':
     import argparse
     parser = argparse.ArgumentParser()
-    parser.add_argument('-f', '--file', type=argparse.FileType('r', encoding='utf8'), default=None, help="In file in CSV format" )
+    parser.add_argument('-f', '--infile', type=argparse.FileType('r', encoding='utf8'), default=None, help="In file in CSV format" )
     args = parser.parse_args()
 
-    if not args.file:
-        with open("C:\\disk\\ecuboost.eu\\auto_databases_one_March_2018_en.csv", encoding="utf8", newline="") as fh:
-            fhcars = fh.read()
+    if not args.infile:
+        with open("auto_databases_one_March_2018_en.csv", encoding="utf8", newline="") as fh:
+            cars = fh.read()
+    else:
+        cars = infile.read()
 
-    reader = csv.DictReader(io.StringIO(fhcars), delimiter=',', dialect='excel')
+    reader = csv.DictReader(io.StringIO(cars), delimiter=',', dialect='excel')
 
-    print(get_car_info(reader)['Talbot'])
+    write_car_csv(get_car_info(reader))
